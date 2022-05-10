98412357eb5b6eb73a00a22c30da303d602650d2
OpenSauce <lawleycobhc@gmail.com>
2022-05-10 18:29:20 +0100

    Watches people

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 use std::env;
 
+use chrono::{DateTime, Duration, Utc};
 use dotenv;
 use serenity::async_trait;
 use serenity::model::channel::Message;
+use serenity::model::gateway::Activity;
 use serenity::model::gateway::Ready;
 use serenity::prelude::*;
 
@@ -16,6 +18,8 @@ impl EventHandler for Handler {
     // Event handlers are dispatched through a threadpool, and so multiple
     // events can be dispatched simultaneously.
     async fn message(&self, ctx: Context, msg: Message) {
+        ctx.set_activity(Activity::watching(msg.author.name)).await;
+
         if msg.content == "!ping" {
             // Sending a message can fail, due to a network error, an
             // authentication error, or lack of permissions to post in the
@@ -27,14 +31,18 @@ impl EventHandler for Handler {
         }
     }
 
+    // let start = Instant::now()
+    // let duration = start.elapsed()
+
     // Set a handler to be called on the `ready` event. This is called when a
     // shard is booted, and a READY payload is sent by Discord. This payload
     // contains data like the current user's guild Ids, current user data,
     // private channels, and more.
     //
     // In this case, just print what the current user's username is.
-    async fn ready(&self, _: Context, ready: Ready) {
+    async fn ready(&self, ctx: Context, ready: Ready) {
         println!("{} is connected!", ready.user.name);
+        ctx.set_activity(Activity::watching("Rusty Anime")).await;
     }
 }
 
@@ -50,7 +58,7 @@ async fn main() {
             token = v;
             println!("Hi {}", token)
         }
-        Err(e) => {
+        Err(_e) => {
             token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
         }
     };
